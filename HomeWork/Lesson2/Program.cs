d1f2d3c7e927af717e2453604fb5b8c8541f7344
Irina <luchkina-irina@mail.ru>
2022-12-07 19:11:43 +0300

    Exercise 1 added

---
+++ HomeWork/Lesson2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+int Findnum(int num)
+{
+    int des = num / 10;
+    int ed = des % 10;
+    return ed;
+}
+
+Console.Write("Input a threedigit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = Findnum(num);
+Console.WriteLine(result);
+*/
