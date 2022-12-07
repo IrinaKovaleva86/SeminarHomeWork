5c46bf399865f30d1674f3b2fb4c3f0304911b1a
Irina <luchkina-irina@mail.ru>
2022-12-07 19:15:25 +0300

    Exercise 3 added

--- HomeWork/Lesson2/Program.cs
+++ HomeWork/Lesson2/Program.cs
@@ -36,3 +36,29 @@ Console.Write("Input a number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int number = ThreeDigit(num);
 Console.WriteLine(number);
+
+//Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+bool Days(int num)
+{
+    if(num > 0 && num < 8)
+    {
+        if(num == 6 || num == 7)
+        {
+            Console.Write("Выходной");
+        }
+        else
+        {
+            Console.Write("Рабочий день");
+        }
+    }
+    else
+    {    
+        Console.Write("Не верное число");
+    }
+    return true;
+}
+
+Console.Write("Input a number from 1 to 7: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Days(num);
