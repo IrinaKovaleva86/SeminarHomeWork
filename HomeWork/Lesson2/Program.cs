2b0d86b94c664e4774e99c99bdd739419e4850ce
Irina <luchkina-irina@mail.ru>
2022-12-07 19:14:04 +0300

    Exercise 2 added

--- HomeWork/Lesson2/Program.cs
+++ HomeWork/Lesson2/Program.cs
@@ -12,3 +12,27 @@ int num = Convert.ToInt32(Console.ReadLine());
 int result = Findnum(num);
 Console.WriteLine(result);
 */
+
+// Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int ThreeDigit(int num)
+{
+    int result = -1;
+    if (num >= 100)
+    {
+        while(num >999)
+        {
+            num = num / 10;
+        }
+            result = num % 10;
+    }
+    else
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
+    return result;
+}
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int number = ThreeDigit(num);
+Console.WriteLine(number);
