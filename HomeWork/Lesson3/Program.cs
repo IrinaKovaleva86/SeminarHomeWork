0f5b57eef6bf57760cc5401917fa194f741d8ec7
Irina <luchkina-irina@mail.ru>
2022-12-09 15:34:05 +0300

    Exercise 1 added

---
+++ HomeWork/Lesson3/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 1. Напишите программу, которая принимает на вход число и проверяет, является ли оно палиндромом.
+
+/*
+bool Palindrom(int number)
+{
+    int n = number;
+    int temp = 0;
+    int revNum = 0;
+    while(n > 0)
+        {
+            temp = n % 10;
+            revNum = revNum*10 + temp;
+            n /= 10;
+        }
+
+    n = number;
+    if (n == revNum)
+        {
+            Console.WriteLine("Yes");
+            return true;
+        }
+    else
+        {
+            Console.WriteLine("No");
+            return false;
+        }
+}
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Palindrom(num);
+*/
