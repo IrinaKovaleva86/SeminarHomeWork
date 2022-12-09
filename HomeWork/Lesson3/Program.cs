16d6ed655681a743b4a717cce6bb9f5c4c4b0136
Irina <luchkina-irina@mail.ru>
2022-12-09 15:44:17 +0300

    Exercise 2 added

--- HomeWork/Lesson3/Program.cs
+++ HomeWork/Lesson3/Program.cs
@@ -29,3 +29,32 @@ Console.Write("Input a number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 Palindrom(num);
 */
+
+//Задача 2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+
+/*
+double Distance(double xa, double ya, double za, double xb, double yb, double zb)
+{
+    double x = xb - xa;
+    double y = yb - ya;
+    double z = zb - za;
+    double result = Math.Sqrt(Math.Pow(x,2)+Math.Pow(y,2)+Math.Pow(z,2));
+    return result;
+}
+
+Console.Write("Введите xA: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите yA: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите zA: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите xB: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите yB: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите zB: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double dist = Distance(x1, y1, z1, x2, y2, z2);
+Console.WriteLine("Расстояние между точками " + dist);
+*/
