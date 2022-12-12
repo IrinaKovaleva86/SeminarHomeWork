acec461562cffeacc41f3f85ffad04f94019b285
Irina <luchkina-irina@mail.ru>
2022-12-12 16:14:50 +0300

    exercise 1 added

---
+++ HomeWork/Lesson4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*
+int Degree(int a, int b)
+{
+    int deg = a;
+    for(int current = 1;current < b; current++)
+    {
+        deg = deg*a;
+    }
+    return deg;
+}
+Console.Write(("Input a number A: "));
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write(("Input a number B: "));
+int numB = Convert.ToInt32(Console.ReadLine());
+int deg = Degree(numA, numB);
+Console.WriteLine ($"A в степени B равно {deg}");
+*/
