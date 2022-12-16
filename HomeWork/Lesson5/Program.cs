bc481e561191db847247041fc9b8e603e0513a8b
Irina <luchkina-irina@mail.ru>
2022-12-16 13:46:23 +0300

    Exercise 2 added

--- HomeWork/Lesson5/Program.cs
+++ HomeWork/Lesson5/Program.cs
@@ -39,3 +39,26 @@ ShowArray(myArray);
 int even = EvenNumber(myArray);
 Console.WriteLine(even);
 */
+
+// Задача 2. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int Sum(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 == 1)
+            sum  += array[i];
+    }
+    return sum;
+}
+Console.Write("Input a number of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandomArray(size,min,max);
+ShowArray(myArray);
+int sum = Sum(myArray);
+Console.WriteLine(sum);
