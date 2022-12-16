3759cd1e63903860afdf4ba81cd19562872af344
Irina <luchkina-irina@mail.ru>
2022-12-16 13:11:23 +0300

    Metods createArray and showArray added

---
+++ HomeWork/Lesson5/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+        for(int i=0; i < size; i++)
+            array[i] = new Random().Next(minValue, maxValue);
+    
+    return array;
+
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+    Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
