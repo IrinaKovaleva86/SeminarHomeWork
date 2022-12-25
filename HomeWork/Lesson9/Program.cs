36a84e7e23c761142b6a77f3c46653f4474aada8
Irina <luchkina-irina@mail.ru>
2022-12-25 16:39:45 +0300

    Exercise 2 added

--- HomeWork/Lesson9/Program.cs
+++ HomeWork/Lesson9/Program.cs
@@ -10,3 +10,19 @@ Console.Write("Input a number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 ShowNumbers(num);
 */
+
+// 1. Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+/*
+int SumOfNumbers(int m, int n)
+{
+    if(n!=m) return SumOfNumbers(m,n-1) + n;
+    else return m;
+}
+
+Console.Write("Input the first number: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the second number: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+SumOfNumbers(numM,numN);
+Console.Write(SumOfNumbers(numM,numN));
+*/
