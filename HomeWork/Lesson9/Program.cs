ded288790a61d347d80ae25e3b138df58494dc37
Irina <luchkina-irina@mail.ru>
2022-12-25 16:24:46 +0300

    Exercise 1 added

---
+++ HomeWork/Lesson9/Program.cs
@@ -0,0 +1,12 @@
+﻿// 1. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+/*
+void ShowNumbers(int n)
+{
+    Console.Write(n +" ");
+    if(n>1) ShowNumbers(n-1);
+}
+
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+ShowNumbers(num);
+*/
